74c2cef141fc0b4207efdc3eff03e4374689a200
A M Rizal <34767112+amrizal94@users.noreply.github.com>
2023-10-11 15:52:38 +0700

    Latihan LinearLayout, RelativeLayout, FrameLayout, dan TableLayout Tujuan

---
+++ app/src/main/java/com/amrizal94/myviewandviewgroup/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.amrizal94.myviewandviewgroup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
